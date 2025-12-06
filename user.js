9049bd08cbce6d420608896aa896c6eeacf0fd92
Alex11-del <alexa.08.chizh@gmail.com>
2025-12-06 17:07:37 +0200

    added sellers and users

---
+++ user.js
@@ -0,0 +1,15 @@
+const urlParams = new URLSearchParams(window.location.search);
+const id = urlParams.get('id');
+let profile = document.getElementById('profile');
+let url = 'https://my-json-server.typicode.com/RobocodeSchool/marketplace';
+//user details request
+fetch(`${url}/users/${id}`)
+.then(async function(response) {
+    let user = await response.json();
+    profile.innerHRML = `
+    <h1>${user.name}</h1>
+    <h2>${user.sirname}</h2>
+    <img class="profile-img" src = "${user.photo_url}">
+    <p>Balance: ${user.balance}$</p>
+    `
+})
